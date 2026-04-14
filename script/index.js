f5b70684418a6fe960306a6a551872b9c12aea82
sabbycorrie-cyber <sabbycorrie@gmail.com>
2026-04-14 22:47:08 +0200

    "JS - Added user price input, converted it to a number, applied 10% discount followed by a display of the result."

--- script/index.js
+++ script/index.js
@@ -1,9 +1,12 @@
 // Assignment 2 //
 
 console.log ("Bid a price plus recieve a 10% discount on this item!");
-prompt ("Bid your price $");
-let input = prompt ("Bid your price $");
-let amount = Number(input);
-newPrice = amount % 10;
 
-alert ("Congratulations! with your discount, you only have to pay ${newPrice} for this item!")
+alert ("Bid a price plus recieve a 10% discount on this item!");
+
+let promptInput = Number(prompt("Bid your price $"));
+
+let discount = promptInput * 10 / 100;
+let newPrice = promptInput - discount;
+
+alert (`Congratulations! with your discount, you only have to pay: $${newPrice} for this item!`)
